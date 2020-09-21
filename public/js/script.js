8ec7b9d9ab37ca238f139ca1c45158484088bcfb
Anisa <aagermanova@gmail.com>
2020-09-20 18:54:54 -0700

    slider-dotshead

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,10 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
     nextArrow:'<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-dotshead',
   });
 
-  $('.slider-dots').slick({
+  $('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
